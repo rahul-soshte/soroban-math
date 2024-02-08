f7257950c8a765ac93d865b48098df3ce9bbc4da
root <rahul.soshte4771@gmail.com>
2024-02-08 09:39:22 +0000

    generic traits for soronum type

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,40 @@
-pub mod i32;
-pub mod u32;
+pub struct SoroNum<T> {
+    value: T,
+}
+
+impl<T> SoroNum<T> {
+    // Constructor
+    pub fn new(value: T) -> Self {
+        Self { value }
+    }
+    
+    // Example basic method (you'll replace this with actual arithmetic operations)
+    pub fn value(&self) -> &T {
+        &self.value
+    }
+}
+
+pub trait CoreArith {
+    fn add(self, other: Self) -> Self;
+    fn sub(self, other: Self) -> Self;
+    fn mul(self, other: Self) -> Self;
+    fn div(self, other: Self) -> Self;
+}
+
+impl CoreArith for SoroNum<i32> {
+    fn add(self, other: Self) -> Self {
+        SoroNum { value: self.value + other.value }
+    }
+
+    fn sub(self, other: Self) -> Self {
+        SoroNum { value: self.value - other.value }
+    }
+
+    fn mul(self, other: Self) -> Self {
+        SoroNum { value: self.value * other.value }
+    }
+
+    fn div(self, other: Self) -> Self {
+        SoroNum { value: self.value / other.value }
+    }
+}
