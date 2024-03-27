6074ac9eeb1aec3fce53ab59cf0b8aa034e6f797
root <rahul.soshte4771@gmail.com>
2024-03-27 15:34:42 +0000

    fix code

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-pub mod core_arith;
+use soroban_sdk::{U256, I256, TryFromVal, Env, ConversionError, Val};
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct SoroNum<T> {
     value: T,
 }
 
+
+
 impl<T> SoroNum<T> {
-    // Constructor
     pub fn new(value: T) -> Self {
         Self { value }
     }
@@ -23,3 +24,140 @@ pub trait CoreArith {
     fn div(self, other: Self) -> Self;
 }
 
+
+
+
+// Core Math Functions
+// i32
+impl CoreArith for SoroNum<i32> {
+    fn add(self, other: Self) -> Self {
+        SoroNum { value: &self.value + &other.value }
+    }
+    fn sub(self, other: Self) -> Self {
+        SoroNum { value: &self.value - &other.value } 
+    }
+
+    fn mul(self, other: Self) -> Self {
+        SoroNum { value: &self.value * &other.value }
+    }
+    fn div(self, other: Self) -> Self {
+        SoroNum { value: &self.value / &other.value }
+    }
+}
+
+// u32
+impl CoreArith for SoroNum<u32> {
+    fn add(self, other: Self) -> Self {
+        SoroNum { value: self.value + other.value }
+    }
+    fn sub(self, other: Self) -> Self {
+        SoroNum { value: self.value - other.value }
+    }
+
+    fn mul(self, other: Self) -> Self {
+        SoroNum { value: self.value * other.value }
+    }
+
+    fn div(self, other: Self) -> Self {
+        SoroNum { value: self.value / other.value }
+    }
+
+}
+
+// u64
+impl CoreArith for SoroNum<u64> {
+    fn add(self, other: Self) -> Self {
+        SoroNum { value: self.value + other.value }
+    }
+    fn sub(self, other: Self) -> Self {
+        SoroNum { value: self.value - other.value } 
+    }
+
+    fn mul(self, other: Self) -> Self {
+        SoroNum { value: self.value * other.value }
+    }
+    fn div(self, other: Self) -> Self {
+        SoroNum { value: self.value / other.value }
+    }
+}
+
+// i64
+impl CoreArith for SoroNum<i64> {
+    fn add(self, other: Self) -> Self {
+        SoroNum { value: self.value + other.value }
+    }
+    fn sub(self, other: Self) -> Self {
+        SoroNum { value: self.value - other.value } 
+    }
+    fn mul(self, other: Self) -> Self {
+        SoroNum { value: self.value * other.value }
+    }
+    fn div(self, other: Self) -> Self {
+        SoroNum { value: self.value / other.value }
+    }
+}
+
+// i128
+impl CoreArith for SoroNum<i128> {
+    fn add(self, other: Self) -> Self {
+        SoroNum { value: self.value + other.value }
+    }
+    fn sub(self, other: Self) -> Self {
+        SoroNum { value: self.value - other.value }
+    }
+    fn mul(self, other: Self) -> Self {
+        SoroNum { value: self.value * other.value }
+    }
+    fn div(self, other: Self) -> Self {
+        SoroNum { value: self.value / other.value }
+    }
+}
+
+// u128
+impl CoreArith for SoroNum<u128> {
+    fn add(self, other: Self) -> Self {
+        SoroNum { value: self.value + other.value }
+    }
+    fn sub(self, other: Self) -> Self {
+        SoroNum { value: self.value - other.value } 
+    }
+    fn mul(self, other: Self) -> Self {
+        SoroNum { value: self.value * other.value }
+    }
+    fn div(self, other: Self) -> Self {
+        SoroNum { value: self.value / other.value }
+    }
+}
+
+
+// U256
+impl CoreArith for SoroNum<U256> {
+    fn add(self, other: Self) -> Self {
+        SoroNum { value: self.value.add(&other.value) }
+    }
+    fn sub(self, other: Self) -> Self {
+        SoroNum { value: self.value.sub(&other.value) } 
+    }
+    fn mul(self, other: Self) -> Self {
+        SoroNum { value: self.value.mul(&other.value) }
+    }
+    fn div(self, other: Self) -> Self {
+        SoroNum { value: self.value.div(&other.value) }
+    }
+}
+
+// I256
+impl CoreArith for SoroNum<I256> {
+    fn add(self, other: Self) -> Self {
+        SoroNum { value: self.value.add(&other.value) }
+    }
+    fn sub(self, other: Self) -> Self {
+        SoroNum { value: self.value.sub(&other.value) } 
+    }
+    fn mul(self, other: Self) -> Self {
+        SoroNum { value: self.value.mul(&other.value) }
+    }
+    fn div(self, other: Self) -> Self {
+        SoroNum { value: self.value.div(&other.value) }
+    }
+}
